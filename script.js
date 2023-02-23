8fd5aa75a13f196040418a8d8541650e87c0d80f
Sasha <45601549+ohmyhorus@users.noreply.github.com>
2023-02-23 22:18:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const x = 3;
+const y = 5;
+
+if (x > 3 && y < 10) {
+    console.log ('X è maggiore di 3 e y è minore di 10');
+} else {
+    console.log ('X non è maggiore di 3 oppure y non è minore di 10');
+}
+
+if (x > 3 || y <10) {
+    console.log ('X è maggiore di 3 oppure y è minore di 10');
+} else {
+    console.log('X non è maggiore di 3 e y non è minore di 10');
+}
