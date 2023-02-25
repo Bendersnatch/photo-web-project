d3e75255603b1559f472e11d35dfc536f16c7e15
Sasha <45601549+ohmyhorus@users.noreply.github.com>
2023-02-25 22:32:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const backBtn = document.getElementById ('backBtn');
 backBtn.addEventListener('click', function () {
    window.scrollTo (0, 0);
 });
+
+window.addEventListener ('scroll', function() {
+    let posY = this.window.scrollY;
+
+    if (posY > 0) {
+        backBtn.style.display = 'block';
+    } else {
+        backBtn.style.display = 'none';
+    }
+});
