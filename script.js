d8a76a6c0e00d1c76e0abc238ab2fa75a2658003
Sasha <45601549+ohmyhorus@users.noreply.github.com>
2023-02-25 22:14:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-const x = 3;
-const y = 5;
+const backBtn = document.getElementById ('backBtn');
 
-if (x > 3 && y < 10) {
-    console.log ('X è maggiore di 3 e y è minore di 10');
-} else {
-    console.log ('X non è maggiore di 3 oppure y non è minore di 10');
-}
-
-if (x > 3 || y <10) {
-    console.log ('X è maggiore di 3 oppure y è minore di 10');
-} else {
-    console.log('X non è maggiore di 3 e y non è minore di 10');
-}
+backBtn.addEventListener('click', function () {
+   window.scrollTo (0, 0);
+});
